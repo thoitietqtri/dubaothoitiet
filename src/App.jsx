b65d68aa368d376c43a436108d9e254ada129776
thoitietqtri <ngoduong28093@gmail.com>
2026-06-03 16:49:37 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import MapComponent from "./components/MapComponent";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="main-title">
+       DỰ BÁO THỜI TIẾT CHO XÃ/PHƯỜNG TỈNH QUẢNG TRỊ
+      </h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
